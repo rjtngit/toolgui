cfe0ee57ffeb828e1a5a69cb938cf666cb22954d
Justin Rempel <justinrempel@gmail.com>
2021-03-07 20:45:02 -0800

    Simplify example

--- examples/example.py
+++ examples/example.py
@@ -2,34 +2,22 @@ import imgui
 import toolgui
 
 
-class Example:
-    custom_window_enabled = False
-    test_window_enabled = False
+class State:
+    window_enabled = False
 
 
-@toolgui.menu_item("Examples/Show Custom Window")
-def show_custom_window():
-    Example.custom_window_enabled = True
+@toolgui.menu_item("Example/Hello World")
+def show_window():
+    State.window_enabled = True
 
 
 @toolgui.on_update()
-def update_custom_window():
-    if Example.custom_window_enabled:
-        expanded, opened = imgui.begin("Custom window", True)
-        Example.custom_window_enabled = opened
-        imgui.text("Hello custom window")
+def update_window():
+    if State.window_enabled:
+        expanded, opened = imgui.begin("Hello World", True)
+        State.window_enabled = opened
+        imgui.text("Hello world")
         imgui.end()
-
-
-@toolgui.menu_item("Examples/Show Test Window")
-def show_test_window():
-    Example.test_window_enabled = True
-
-
-@toolgui.on_update()
-def update_test_window():
-    if Example.test_window_enabled:
-        imgui.show_test_window()
 
 
 toolgui.start_toolgui_app()
