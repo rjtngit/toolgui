72bb18b1016632811aaaac47f64fe2442b52a1c6
Justin Rempel <justinrempel@gmail.com>
2021-03-07 22:09:32 -0800

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="toolgui",
-    version="0.0.2",
+    version="0.0.3",
     description="toolgui",
     packages=find_packages(),
     long_description=long_description,
