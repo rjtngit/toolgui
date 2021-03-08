69446d3f8cba1adc85b9dc692f8d3af0942b3378
Justin Rempel <justinrempel@gmail.com>
2021-03-07 21:59:01 -0800

    fix imports

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -7,7 +7,7 @@ setup(
     name="toolgui",
     version="0.0.2",
     description="toolgui",
-    package_dir={"": "toolgui"},
+    packages=find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rempelj/toolgui",
--- toolgui/__init__.py
+++ toolgui/__init__.py
@@ -1,4 +1,3 @@
 from ._app import start_toolgui_app
 from ._app import on_update
 from ._menu import menu_item
-
